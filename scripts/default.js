9c0c5e51262c262d30c01dd44f5b0a4d34912fed
Frank <52516263+frankchaparro@users.noreply.github.com>
2020-03-12 16:52:12 -0400

    Add files via upload

--- scripts/default.js
+++ scripts/default.js
@@ -1,16 +1,17 @@
 var userid = prompt("What is your Student login at CPCC?");
 switch (userid)
 { 
-case 1: console.log("flopez10");
+case "flopez10":
 var output = "Your Student ID is: " + userid + "!";
 alert(output);
 studentURL = "http://students.cpcc.edu/~"
 alert(studentURL + userid); 
 break; 
-case 2: console.log("value of i = 2"); 
+case "mkhan016": 
+console.log("value of i = 2"); 
 break; 
 case 3: console.log("value of i = 3"); 
 break; 
 default: console.log("userid is not equal to any given values"); 
 break;
-} 
+}
